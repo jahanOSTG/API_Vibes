5434ca509d11f65c5ecd2df2248407e54fddbab9
Faria Jahan Janie <janiefaria315@gmail.com>
2025-03-28 20:43:20 +0600

    Add files via upload

---
+++ jquery/jquery.js
@@ -0,0 +1,44 @@
+// 4 ways to call api - XMLHttpRequest, fetch, axios, jquery
+// add jquery libraray cdn
+// ajax - asynchronous javascript and xml
+
+console.clear();
+
+const makeRequest = async (url, method, data) => {
+  try {
+    const result = await $.ajax({
+      url: url,
+      method: method,
+      data: data,
+    });
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+//deleteData
+
+const deleteData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", "DELETE").then(
+    (res) => console.log(res)
+  );
+};
+deleteData();
+//create data
+const createData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", "PUT", {
+    id: 1,
+    title: "fooma",
+    body: "barma",
+    userId: 1,
+  }).then((res) => console.log(res));
+};
+createData();
+//getdata
+
+const getData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", "GET").then(
+    (res) => console.log(res)
+  );
+};
+getData();
